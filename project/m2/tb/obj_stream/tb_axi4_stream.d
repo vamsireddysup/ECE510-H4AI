5cb3324740aa400e3cb128488b83b7f383d9895d
Vamsidhar Reddy Eraganeni <vamsireddysup@gmail.com>
2026-04-29 23:47:22 -0700

    m2: remove obj dirs and VCD files from tracking, update gitignore

--- project/m2/tb/obj_stream/tb_axi4_stream.d
+++
@@ -1,9 +0,0 @@
-tb_axi4_stream.o: ../tb_axi4_stream.cpp Vaxi4_stream.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h \
- /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_trace.h
