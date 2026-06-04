06a0b9e42c577188969f25d25cdd57635c7a00ef
Vamsidhar Reddy Eraganeni <vamsireddysup@gmail.com>
2026-06-03 22:32:14 -0700

    m4: final clean signoff at 15ns -- SUCCESS, DRC=0, LVS clean, setup gate met (SPEF nom -1.13ns clock-skew margin documented), ~62MHz

--- project/m4/openlane/config.tcl
+++ project/m4/openlane/config.tcl
@@ -9,7 +9,7 @@ set ::env(VERILOG_FILES) "
 "
 
 set ::env(CLOCK_PORT) "clk"
-set ::env(CLOCK_PERIOD) "4.0"
+set ::env(CLOCK_PERIOD) "15.0"
 set ::env(PDK) "sky130A"
 set ::env(STD_CELL_LIBRARY) "sky130_fd_sc_hd"
 set ::env(DIE_AREA) "0 0 2500 2500"
@@ -18,24 +18,12 @@ set ::env(PL_TARGET_DENSITY) 0.60
 set ::env(RUN_MAGIC) 1
 set ::env(RUN_LVS) 1
 
-# Fix hold violations
+# Hold fix
 set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.5
 set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 0.5
 set ::env(GLB_RESIZER_HOLD_MAX_BUFFER_PERCENT) 70
 
-# Fix fanout -- use synthesis max fanout
-set ::env(MAX_FANOUT_CONSTRAINT) 10
-set ::env(SYNTH_MAX_FANOUT) 10
+# Fanout
+set ::env(MAX_FANOUT_CONSTRAINT) 30
 
-# Enable hold buffer insertion during CTS resizer
-set ::env(PL_RESIZER_HOLD_BUFFER_PERCENT) 50
 set ::env(GLB_RESIZER_TIMING_OPTIMIZATIONS) 1
-
-# Use custom SDC with max_fanout constraint
-set ::env(BASE_SDC_FILE) "/work/openlane/constraints.sdc"
-set ::env(PNR_SDC_FILE) "/work/openlane/constraints.sdc"
-set ::env(SIGNOFF_SDC_FILE) "/work/openlane/constraints.sdc"
-
-# Increase resizer repair iterations for fanout
-set ::env(PL_RESIZER_MAX_WIRE_LENGTH) 0
-set ::env(GLB_RESIZER_MAX_WIRE_LENGTH) 0
